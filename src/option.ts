abf9d0d07ecca01578cba54b019f60e9f86a38a4
lloydzhou <lloydzhou@qq.com>
2022-06-28 13:27:53 +0800

    update

--- src/option.ts
+++ src/option.ts
@@ -55,24 +55,7 @@ import {
 
 import { ValueAxisBaseOption, LogAxisBaseOption, CategoryAxisBaseOption, TimeAxisBaseOption, AxisBaseOptionCommon } from 'echarts/types/src/coord/axisCommonTypes'
 
-
-type OptionalPropertyNames<T> =
-  { [K in keyof T]-?: ({} extends { [P in K]: T[K] } ? K : never) }[keyof T];
-
-type SpreadProperties<L, R, K extends keyof L & keyof R> =
-  { [P in K]: L[P] | Exclude<R[P], undefined> };
-
-type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
-
-type SpreadTwo<L, R> = Id<
-  & Pick<L, Exclude<keyof L, keyof R>>
-  & Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>>
-  & Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>>
-  & SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
->;
-
-type Spread<A extends readonly [...any]> = A extends [infer L, ...infer R] ?
-  SpreadTwo<L, Spread<R>> : unknown
+import type { Spread } from './spread'
 
 type AxisBaseOption = Spread<[ValueAxisBaseOption, LogAxisBaseOption, CategoryAxisBaseOption, TimeAxisBaseOption, AxisBaseOptionCommon]>
 
